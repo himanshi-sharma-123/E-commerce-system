6abea91b0e8a876ec87c024ece8cf311b77b71ca
Himanshi Sharma <himanshish456@gmail.com>
2024-08-30 14:09:12 +0530

    ecommerce structure and necessary dependencies

---
+++ src/test/java/com/example/ecommerce_system/EcommerceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
